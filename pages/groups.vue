1dee96e7d71606aad2b9ec67ae332ff6710a4681
budi@x <budi@avasoft.co>
2021-10-01 07:57:25 +0000

    ccc

--- pages/groups.vue
+++ pages/groups.vue
@@ -7,7 +7,10 @@
       class="sticky z-100 border p-5 m-3 top-0 bg-black text-white text-center mx-auto w-4/5 sm:w-4/5 md:w-4/5 lg:w-1/2"
     >
       <p class="m-1 sm:m-3">{{ error }}</p>
-      <button class="button--grey" @click="resetError()">Ok</button>
+      <button
+        class="button--grey"
+        @click="resetError()"
+      >Ok</button>
     </div>
     <div
       v-for="(group, i) in data"
@@ -24,6 +27,14 @@
           <div>
             <p>SRL Level:</p>
             <h3 class="font-semibold text-gray-500">{{ group.srl.level }}</h3>
+            <!-- <h3 class="font-semibold text-gray-500">{{ group.students }}</h3> -->
+          </div>
+          <div
+            v-for="(tmp, i2) in group.students"
+            :key="i2"
+          >
+            <h4>{{ tmp.angkatan }}</h4>
+
           </div>
         </div>
         <div class="flex justify-between w-full">
@@ -31,7 +42,10 @@
           <!-- students ga bisa dipanggil, gua rasa karena 1 group terdiri dari 3 students (kudu v-for?) -->
           <!-- <p>Anggota:</p>
             <h3 class="font-semibold text-gray-500">{{ group.students.angkatan }}</h3> -->
-          <button class="w-32 border rounded mt-2 px-3 py-1 sm:mb-0" @click="readPost(group)">
+          <button
+            class="w-32 border rounded mt-2 px-3 py-1 sm:mb-0"
+            @click="readPost(group)"
+          >
             Detail
           </button>
         </div>
@@ -42,27 +56,27 @@
 <script>
 export default {
   async asyncData({ $strapi, $md }) {
-    const data = await $strapi.$groups.find()
-    return { data }
+    const data = await $strapi.$groups.find();
+    return { data };
   },
   data() {
     return {
-      error: '',
-    }
+      error: "",
+    };
   },
   methods: {
     readPost(group) {
       if (this.$strapi.user) {
-        this.error = ''
-        this.$nuxt.$router.push(`/group/${group.id}`)
+        this.error = "";
+        this.$nuxt.$router.push(`/group/${group.id}`);
       } else {
-        this.error = 'Please Login to read groups'
+        this.error = "Please Login to read groups";
       }
     },
     resetError() {
-      this.error = ''
+      this.error = "";
     },
   },
-}
+};
 </script>
 <style></style>
